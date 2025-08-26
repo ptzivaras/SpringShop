4133b0693d57de5831927f3aae1963f28a85cbd7
ptzivaras <82588420+ptzivaras@users.noreply.github.com>
2025-08-26 23:10:54 +0300

    setup Redux store and Provider

---
+++ eshop-ui/src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+// Redux
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* Provider = κάνει διαθέσιμο το Redux store σε όλη την React εφαρμογή */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+)
